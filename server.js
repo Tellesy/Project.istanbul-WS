799f4ea52343175cf7ca30f35e0bfd9cf37bd01b
Istanbul <mtellesy@Project.Exdus>
2017-12-12 13:56:48 +0200

    update

---
+++ server.js
@@ -0,0 +1,50 @@
+//Dependencies
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+
+var app = express();
+
+// Mrogran logger is HTTP request logger middleware for node.js
+app.use(logger('dev'));
+// The body-parser is used to convert the respons into JSON
+app.use(bodyParser.json());
+
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+// Auth Middleware - This will check if the token is valid
+// Only the requests that start with /api/v1/* will be checked for the token.
+// Any URL's that do not follow the below pattern should be avoided unless you
+// are sure that authentication is not needed
+app.all('/api/v1/*', [require('./middlewares/validateRequest')]);
+
+app.use('/', require('./routes'));
+
+// If no route is matched by now, it must be a 404
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Start the server
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+//
